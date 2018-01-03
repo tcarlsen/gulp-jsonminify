7d5da6a69a0e8b2b9e041ad14d356ef6baaba6b6
idleberg <jan@fragnebenan.com>
2018-01-03 17:32:41 +0100

    remove dependency on deprecated gulp-util

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var through = require('through2');
-var gutil = require('gulp-util');
+var PluginError = require('plugin-error');
 var jsonminify = require('jsonminify');
 
 module.exports = function () {
     'use strict';
-    
+
     return through.obj(function (file, encoding, callback) {
         if (file.isNull()) {
             this.push(file);
@@ -12,14 +12,14 @@ module.exports = function () {
         }
 
         if (file.isStream()) {
-            this.emit('error', new gutil.PluginError('gulp-jsonminify', 'Streaming not supported'));
+            this.emit('error', new PluginError('gulp-jsonminify', 'Streaming not supported'));
             return callback();
         }
 
         try {
             file.contents = new Buffer(jsonminify(file.contents.toString()).toString());
         } catch (err) {
-            this.emit('error', new gutil.PluginError('gulp-jsonminify', err));
+            this.emit('error', new PluginError('gulp-jsonminify', err));
         }
 
         this.push(file);
